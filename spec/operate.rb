6a9b0dfdfea5e76bb098ec379c384fb99f7a533d
Ernie Prabhakar <ernest.prabhakar@gmail.com>
2016-04-05 18:54:11 -0700

    Handle all sample input
    Resolve issue #2

--- spec/operate.rb
+++ spec/operate.rb
@@ -7,7 +7,7 @@ RSpec.describe "Operate" do
     @rpn_eval = RPNEval.new
   end
   
-  it "Adds two numbers" do
+  it "Adds two numbers (sample #1)" do
     @rpn_eval.call "5"
     @rpn_eval.call "8"
     result = @rpn_eval.call "+"
@@ -33,6 +33,33 @@ RSpec.describe "Operate" do
     @rpn_eval.call "8"
     result = @rpn_eval.call "/"
     expect(result).to eq 0.625
+  end
+  
+  it "passes sample input #2" do
+    @rpn_eval.call "-3"
+    @rpn_eval.call "-2"
+    @rpn_eval.call "*"
+    @rpn_eval.call "5"
+    result = @rpn_eval.call "+"
+    expect(result).to eq 11
+  end
+
+  it "passes sample input #3" do
+    @rpn_eval.call "2"
+    @rpn_eval.call "9"
+    @rpn_eval.call "3"
+    @rpn_eval.call "+"
+    result = @rpn_eval.call "*"
+    expect(result).to eq 24
+  end
+
+  it "passes sample input #4" do
+    @rpn_eval.call "20"
+    @rpn_eval.call "13"
+    @rpn_eval.call "-"
+    @rpn_eval.call "2"
+    result = @rpn_eval.call "/"
+    expect(result).to eq 3.5
   end
 
 end
