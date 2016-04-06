1df0c86a2ad0cc38c4d1a588914612ed07e21522
Ernie Prabhakar <ernest.prabhakar@gmail.com>
2016-04-06 06:34:56 -0700

    Make specs use rpn_init

--- spec/operate.rb
+++ spec/operate.rb
@@ -1,64 +1,64 @@
 # Operator tests
 
-require 'rpn_eval'
+require 'rpn_init'
 
 RSpec.describe "Operate" do
   before do
-    @rpn_eval = RPNEval.new
+    @rpn = rpn_init()
   end
   
   it "Adds two numbers (sample #1)" do
-    @rpn_eval.call "5"
-    @rpn_eval.call "8"
-    result = @rpn_eval.call "+"
+    @rpn.call "5"
+    @rpn.call "8"
+    result = @rpn.call "+"
     expect(result).to eq 13
   end
 
   it "Subtracts two numbers" do
-    @rpn_eval.call "5"
-    @rpn_eval.call "8"
-    result = @rpn_eval.call "-"
+    @rpn.call "5"
+    @rpn.call "8"
+    result = @rpn.call "-"
     expect(result).to eq -3
   end
 
   it "Multiplies two numbers" do
-    @rpn_eval.call "5"
-    @rpn_eval.call "8"
-    result = @rpn_eval.call "*"
+    @rpn.call "5"
+    @rpn.call "8"
+    result = @rpn.call "*"
     expect(result).to eq 40
   end
 
   it "Divides two numbers" do
-    @rpn_eval.call "5"
-    @rpn_eval.call "8"
-    result = @rpn_eval.call "/"
+    @rpn.call "5"
+    @rpn.call "8"
+    result = @rpn.call "/"
     expect(result).to eq 0.625
   end
   
   it "passes sample input #2" do
-    @rpn_eval.call "-3"
-    @rpn_eval.call "-2"
-    @rpn_eval.call "*"
-    @rpn_eval.call "5"
-    result = @rpn_eval.call "+"
+    @rpn.call "-3"
+    @rpn.call "-2"
+    @rpn.call "*"
+    @rpn.call "5"
+    result = @rpn.call "+"
     expect(result).to eq 11
   end
 
   it "passes sample input #3" do
-    @rpn_eval.call "2"
-    @rpn_eval.call "9"
-    @rpn_eval.call "3"
-    @rpn_eval.call "+"
-    result = @rpn_eval.call "*"
+    @rpn.call "2"
+    @rpn.call "9"
+    @rpn.call "3"
+    @rpn.call "+"
+    result = @rpn.call "*"
     expect(result).to eq 24
   end
 
   it "passes sample input #4" do
-    @rpn_eval.call "20"
-    @rpn_eval.call "13"
-    @rpn_eval.call "-"
-    @rpn_eval.call "2"
-    result = @rpn_eval.call "/"
+    @rpn.call "20"
+    @rpn.call "13"
+    @rpn.call "-"
+    @rpn.call "2"
+    result = @rpn.call "/"
     expect(result).to eq 3.5
   end
 
