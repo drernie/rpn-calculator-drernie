96ffb1c9c779720a32902ee9da45b017d2436a37
Ernie Prabhakar <ernest.prabhakar@gmail.com>
2016-04-05 18:33:25 -0700

    Parse on the stack

--- lib/rpn_eval.rb
+++ lib/rpn_eval.rb
@@ -1,13 +1,24 @@
 # Stub for testing
 
+# Parser Regex from http://stackoverflow.com/questions/8071533/convert-input-value-to-integer-or-float-as-appropriate-using-ruby
+
 class RPNEval
   attr :stack
   def initialize()
-    stack = []
+    @stack = []
   end
   
-  def call(number)
-    value = Integer(number)
-    value
+  def call(input)
+    value = case(input)
+    when /\A\s*[+-]?\d+\.\d+\z/
+        @stack << input.to_f
+    when /\A\s*[+-]?\d+(\.\d+)?[eE]\d+\z/
+        @stack << input.to_f
+    when /\A\s*[+-]?\d+\z/ 
+        @stack << input.to_i
+    else 
+      return "Error: Unknown input: #{input}"
+    end
+    @stack[-1]
   end
 end
--- spec/input.rb
+++ spec/input.rb
@@ -9,4 +9,10 @@ RSpec.describe "Input" do
     expect(result).to eq 4
   end
 
+  it "Takes a decimal string and returns a float" do
+    rpn_eval = RPNEval.new
+    result = rpn_eval.call "5.1"
+    expect(result).to eq 5.1
+  end
+
 end
